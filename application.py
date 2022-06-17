5b4860c618db47e5429958768761a5edf197162d
vikas.reddy <vikas.reddy@mobileheartbeat.com>
2022-06-17 14:00:42 +0530

    Python command line application for searching a file from the file from the path provided by the user.

---
+++ application.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import click
+
+
+@click.command()
+@click.argument('path', default='')
+@click.argument('word', default='')
+def word_search(path, word):
+    """takes the file path and the word from the user and search for existence of the word inside the file from the
+    path """
+    validation(path, word)
+
+    if path.endswith(".txt") | path.endswith(".json"):
+        try:
+            with open(os.path.join(os.path.dirname(__file__), path), 'r') as input_file:
+                content = input_file.read()
+            x = content.__contains__(word)
+            if x:
+                sys.exit("searchWord Found")
+            else:
+                click.echo("searchWord Not found")
+        except FileNotFoundError:
+            click.echo("The file does not exist in the given path")
+    else:
+        click.echo("Please Enter the Valid Path")
+
+
+def validation(path, word):
+    """ validates the path and word provided by the user """
+    if len(path.format(path)) == 0:
+        click.echo("please enter valid path")
+        sys.exit(-1)
+    if len(word) == 0:
+        print("please enter the searchWord")
+        sys.exit(-1)
+
+
+if __name__ == '__main__':
+    word_search()
